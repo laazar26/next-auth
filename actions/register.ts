d9c9a8b5aadafadcbc678b80753376bf4b11a044
laazar26 <116655862+laazar26@users.noreply.github.com>
2024-08-21 16:08:35 +0200

    added server actions and register form

---
+++ actions/register.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import * as z from "zod";
+
+import { RegisterSchema } from "@/schemas";
+
+// export async function login(value: any) {
+//   console.log(value);
+// }
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields" };
+  }
+
+  return { success: "Email sent!" };
+};
